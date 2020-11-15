7616fea0228953071c784922b2890b0a7c8e0843
VikingCanadian <kristhecanadian101@gmail.com>
2020-11-15 15:32:11 -0500

    update

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import os
 
 from search import binary_search_on_suppliers
+from matchstrings import MatchString
 
 if __name__ == '__main__':
     # supplier_name = parse_json(sys.argv[1])
@@ -10,4 +11,7 @@ if __name__ == '__main__':
         os.path.abspath("data/supplierList/supplierlist2-sorted.csv")
     ]
 
-    binary_search_on_suppliers(supplier_lists_path, '$29 Garage Door Openers Repair Alameda Ca 510-214-8005')
+    binary_search_on_suppliers(supplier_lists_path, '$29 Garage Door Openers Repair Alameda Ca 510-214-800')
+    m = MatchString();
+    print(m.match('$29 Garage Door Openers Repair Alameda Ca 510-214-8005', '$29 Garage Door Openers Repai Alameda Ca 510-214-8005'))
+
