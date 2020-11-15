94d37599e2a85f7ebcce4743a84ac6957467abea
Jesse <jessesilber@gmail.com>
2020-11-15 12:54:41 -0500

    name stuff

--- jessestuff.py
+++ jessestuff.py
@@ -3,8 +3,10 @@ import re
 
 
 class SearchObject:
-    phone = ""
-    name = []
+
+    def __init__(self):
+        self.name = []
+        self.phone = ""
 
 
 searchObjects = []
@@ -24,9 +26,14 @@ for i in range(1, 21):
     searchObject = SearchObject()
     searchObject.phone = phone if phone is None else phone.group()
 
+    descSplit = store[0]["description"].split("\n")
+    for j in range(0, 3):
+        if descSplit[j].isnumeric() is False:
+            searchObject.name.append(descSplit[j])
 
     searchObjects.append(searchObject)
 
 for searchObject in searchObjects:
     print(searchObject.phone)
-#     print(searchObject.name)
+    print(searchObject.name)
+    print("----------------------")
