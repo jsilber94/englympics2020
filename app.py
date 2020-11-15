6365ad0de12b8e4de9c4040e0988ac97ada04858
Jesse <jessesilber@gmail.com>
2020-11-15 14:15:45 -0500

    hooked up to app.py, merged code into 1 file

--- app.py
+++ app.py
@@ -3,14 +3,20 @@ import sys
 import numpy as np
 from weighted_levenshtein.clev import dam_lev
 
-from parsing import parse_json
+from process_json import process_json
 from matchstrings import MatchString
 
 
 if __name__ == '__main__':
-    #supplier_name = parse_json(sys.argv[1])
-    #print(supplier_name)
-    transpose_costs = np.ones((128, 128), dtype=np.float64)
-    m = MatchString()
-    print(m.match("HANANA", "BANANA"))
+    # supplier_name = parse_json(sys.argv[1])
+    search_objects = process_json("data/Training Json")
+    for search_object in search_objects:
+        print(search_object.phone)
+        print(search_object.name)
+        print("----------------------")
+
+
+    # transpose_costs = np.ones((128, 128), dtype=np.float64)
+    # m = MatchString()
+    # print(m.match("HANANA", "BANANA"))
 
--- process_json.py
+++ process_json.py
@@ -1,5 +1,6 @@
 import json
 import re
+import os
 from SearchObject import SearchObject
 
 
@@ -15,8 +16,9 @@ def get_phone_number(text_to_parse):
     reg = re.compile("(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}")
     phone = reg.search(str(text_to_parse))
     if phone is None:
-        return re.search("XXX-XXX-XXXX", text_to_parse)
-    return phone
+        return phone
+    else:
+        return ''.join(e for e in phone.group() if e.isalnum())
 
 
 def get_name(text_to_parse):
@@ -28,45 +30,42 @@ def get_name(text_to_parse):
                     re.search('[a-zA-Z]', text_to_parse_split[j]) is not None:
                 names.append(text_to_parse_split[j])
         return names
-    except:
+    except IndexError:
         return names
 
 
-def process_json():
+def process_json(path):
     search_objects = []
+    files = os.listdir(path)
 
-    for i in range(1, 21):
-        try:
-            with open("data/Training Json/j" + str(i) + ".json", "r") as f:
-                json_data = json.load(f)
-
-            search_object = SearchObject()
-
-            try:
-                store = json_data[0]['textAnnotations']
-                text_to_parse = text_annotations(store)
-            except KeyError:
-                store = json_data[0]['fullTextAnnotation']
-                text_to_parse = full_text_annotation(store)
-
-            phone = get_phone_number(text_to_parse)
-            search_object.phone = phone if phone is None else phone.group()
-
-            names = get_name(text_to_parse)
-            search_object.name = names
-
-            search_objects.append(search_object)
-        except IndexError:
-            print("An interesting exception occurred")
-        except:
-            print("An exception occurred")
-
-    for search_object in search_objects:
-        print(search_object.phone)
-        print(search_object.name)
-        print("----------------------")
+    for file in files:
+        file_path = path + "/" + file
+        with open(file_path, "r") as f:
+            json_data = json.load(f)
+        search_objects.append(extract_data(json_data))
 
     return search_objects
 
 
-process_json()
+def extract_data(json_data):
+    try:
+        search_object = SearchObject()
+
+        try:
+            store = json_data[0]['textAnnotations']
+            text_to_parse = text_annotations(store)
+        except KeyError:
+            store = json_data[0]['fullTextAnnotation']
+            text_to_parse = full_text_annotation(store)
+
+        phone = get_phone_number(text_to_parse)
+        search_object.phone = phone
+
+        names = get_name(text_to_parse)
+        search_object.name = names
+
+        return search_object
+    except IndexError:
+        print("An interesting exception occurred")
+
+    return None
