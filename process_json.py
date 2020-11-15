91bbfa3df0d8aa4312319cc8447fa8d10e82502f
Jesse <jessesilber@gmail.com>
2020-11-15 13:49:41 -0500

    more error handling, more cleaned code

--- process_json.py
+++ process_json.py
@@ -11,6 +11,27 @@ def full_text_annotation(store):
     return store["text"]
 
 
+def get_phone_number(text_to_parse):
+    reg = re.compile("(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}")
+    phone = reg.search(str(text_to_parse))
+    if phone is None:
+        return re.search("XXX-XXX-XXXX", text_to_parse)
+    return phone
+
+
+def get_name(text_to_parse):
+    names = []
+    text_to_parse_split = text_to_parse.split('\n')
+    try:
+        for j in range(0, 3):
+            if text_to_parse_split[j].isnumeric() is False and \
+                    re.search('[a-zA-Z]', text_to_parse_split[j]) is not None:
+                names.append(text_to_parse_split[j])
+        return names
+    except:
+        return names
+
+
 def process_json():
     search_objects = []
 
@@ -19,6 +40,8 @@ def process_json():
             with open("data/Training Json/j" + str(i) + ".json", "r") as f:
                 json_data = json.load(f)
 
+            search_object = SearchObject()
+
             try:
                 store = json_data[0]['textAnnotations']
                 text_to_parse = text_annotations(store)
@@ -26,19 +49,11 @@ def process_json():
                 store = json_data[0]['fullTextAnnotation']
                 text_to_parse = full_text_annotation(store)
 
-            reg = re.compile("(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}")
-            phone = reg.search(str(text_to_parse))
-            if phone is None:
-                phone = re.search("XXX-XXX-XXXX", text_to_parse)
-
-            search_object = SearchObject()
+            phone = get_phone_number(text_to_parse)
             search_object.phone = phone if phone is None else phone.group()
 
-            text_to_parse_split = text_to_parse.split('\n')
-            for j in range(0, 3):
-                if text_to_parse_split[j].isnumeric() is False and \
-                        re.search('[a-zA-Z]', text_to_parse_split[j]) is not None:
-                    search_object.name.append(text_to_parse_split[j])
+            names = get_name(text_to_parse)
+            search_object.name = names
 
             search_objects.append(search_object)
         except IndexError:
