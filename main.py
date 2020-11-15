f3bf2663fcaeed39c20be87cf29cf45efa87b58b
Alec Adub <adub.alec@gmail.com>
2020-11-15 17:27:19 -0500

    fix

--- main.py
+++ main.py
@@ -17,22 +17,20 @@ if __name__ == '__main__':
     # binary_search_on_suppliers(supplier_lists_path, '')
     # m = MatchString()
 
-    binary_search_on_suppliers(supplier_lists_path, '3234676664')
-
     # process json and get a list of keywords to match
     # search_objects = process_json(sys.argv[1])
     search_objects = process_json("data/Training Json")
 
     start = time.time()
-    # # iterate through the keywords file by file
-    # for store in search_objects:
-    #     # if store.phone is not None and binary_search_on_suppliers(supplier_lists_path, store.phone, 'phone') is False:
-    #     for name in store.name:
-    #         if binary_search_on_suppliers(supplier_lists_path, name, 'name') is True:
-    #             print(name)
-    #             break
-    # print(time.time() - start)
-    # print("------------------")
+    # iterate through the keywords file by file
+    for store in search_objects:
+        # if store.phone is not None and binary_search_on_suppliers(supplier_lists_path, store.phone, 'phone') is False:
+        for name in store.name:
+            if binary_search_on_suppliers(supplier_lists_path, name, 'name') is True:
+                print(name)
+                break
+    print(time.time() - start)
+    print("------------------")
 
 print(str((time.time() - start) / 60) + " minutes.")
 
